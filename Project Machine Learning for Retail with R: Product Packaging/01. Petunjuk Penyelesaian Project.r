b5f929ada534bbacff34e89146e858324c3c212e
zarinaulfa <35159542+zarinaulfa@users.noreply.github.com>
2021-08-27 11:55:54 +0700

    Create 01. Petunjuk Penyelesaian Project.r

---
+++ Project Machine Learning for Retail with R: Product Packaging/01. Petunjuk Penyelesaian Project.r
@@ -0,0 +1,3 @@
+library(arules)
+transaksi_tabular <- read.transactions(file="https://storage.googleapis.com/dqlab-dataset/transaksi_dqlab_retail.tsv", format="single", sep="\t", cols=c(1,2), skip=1)
+write(transaksi_tabular, file="test_project_retail_1.txt", sep=",")
