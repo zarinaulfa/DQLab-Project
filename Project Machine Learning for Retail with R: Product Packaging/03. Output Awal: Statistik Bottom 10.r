58f4bfc5f865e247fd24186589b9e7928d485d48
zarinaulfa <35159542+zarinaulfa@users.noreply.github.com>
2021-08-27 11:57:27 +0700

    Create 03. Output Awal: Statistik Bottom 10.r

---
+++ Project Machine Learning for Retail with R: Product Packaging/03. Output Awal: Statistik Bottom 10.r
@@ -0,0 +1,7 @@
+library(arules)
+transaksi_tabular <- read.transactions(file="transaksi_dqlab_retail.tsv", format="single", sep="\t", cols=c(1,2), skip=1)
+data_item <- itemFrequency(transaksi_tabular, type="absolute")
+data_item <- sort(data_item, decreasing=FALSE)
+data_item <- data_item[1:10]
+data_item <- data.frame("Nama.Produk"=names(data_item), "Jumlah"=data_item, row.names=NULL)
+write.csv(data_item, file="bottom10_item_retail.txt", sep=",")
